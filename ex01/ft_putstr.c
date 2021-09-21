186253f47c9d64c94acd6af593ea44f55b8153c2
pakucera <pakucera@student.42lausanne.ch>
2021-09-21 06:12:15 +0200

    c04 first commit ex00+ex01

---
+++ ex01/ft_putstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pakucera <pakucera@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/21 04:40:03 by pakucera          #+#    #+#             */
+/*   Updated: 2021/09/21 04:47:37 by pakucera         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int main(void)
+{
+	char *str = "abcde";
+	ft_putstr(str);
+}
